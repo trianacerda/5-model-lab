baee0cd1d545f19e3ebaebb6323b7fba035b58f3
Triana Cerda <trianaRcerda@gmail.com>
2021-10-05 19:41:51 -0700

    /red failing test for /get route for habitat

--- __tests__/habitat.test.js
+++ __tests__/habitat.test.js
@@ -13,10 +13,23 @@ describe('demo routes', () => {
     url: 'https://pokeapi.co/api/v2/pokemon-habitat/1/',
   };
 
-  it('should POST api pokemon to SQL table-- pokemon', () => {
+  it('should POST api habitat to SQL table-- habitat', () => {
     return request(app)
       .post('/api/v2/pokemon-habitat')
       .send(habitat)
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: '1',
+          name: 'cave',
+          url: 'https://pokeapi.co/api/v2/pokemon-habitat/1/',
+        });
+      });
+  });
+
+  it('gets all habitat from SQL table', async () => {
+    await request(app).post('/api/v2/pokemon-habitat').send(habitat);
+    return request(app)
+      .get('/api/v2/pokemon-habitat')
       .then((res) => {
         expect(res.body).toEqual({
           id: '1',
